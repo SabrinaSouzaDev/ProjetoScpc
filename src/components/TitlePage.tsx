43403fecd5fc26dba69e3838457f3a04e7c5ec89
SabrinaSouzaDev <lynxsabri@gmail.com>
2025-01-31 14:24:55 -0300

    refactor: simplify TitlePage component structure and enhance styling

--- src/components/TitlePage.tsx
+++ src/components/TitlePage.tsx
@@ -1,18 +1,13 @@
-import { Route } from 'lucide-react'
-
 type titlePageProps = {
-  title: string
-}
+	title: string;
+};
 
 export const TitlePage = ({ title }: titlePageProps) => {
-  return (
-    <div className="flex items-center gap-4">
-      <div className="flex size-14 items-center justify-center rounded-full bg-cyan-900">
-        <Route className="size-6 text-white sm:size-8" />{' '}
-      </div>
-      <h1 className="text-[1.2rem] sm:text-[1.8rem] md:text-[2.2rem]">
-        {title}
-      </h1>
-    </div>
-  )
-}
+	return (
+		<div className="flex items-center gap-4 border-l-yellow-500 border-l-4 p-4">
+			<h1 className="text-[1.2rem] sm:text-[1.8rem] md:text-[2rem] font-bold tracking-wider">
+				{title}
+			</h1>
+		</div>
+	);
+};
